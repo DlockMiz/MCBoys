525407012c48a1da345242d7ec22ef9082ce3330
Daniel Lock <daniel.w.lock@gmail.com>
2019-07-23 08:52:40 -0500

    add batch scripts

---
+++ mc_server/backup.sh
@@ -0,0 +1,2 @@
+rm ../mc_server_backup -r
+cp ../mc_server ../mc_server_backup -r
---
+++ mc_server/update.sh
@@ -0,0 +1,2 @@
+rm server.jar
+wget https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar
