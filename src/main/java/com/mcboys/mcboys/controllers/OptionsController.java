c6d08d792970c056d1a6cc667a58dff605e9501f
Daniel Lock <daniel.w.lock@gmail.com>
2019-07-24 14:59:39 -0500

    start of backup world functionality

--- src/main/java/com/mcboys/mcboys/controllers/OptionsController.java
+++ src/main/java/com/mcboys/mcboys/controllers/OptionsController.java
@@ -1,18 +1,23 @@
 package com.mcboys.mcboys.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.File;
 
-
 @RestController
 public class OptionsController {
+    @Autowired
+    SimpMessageSendingOperations template;
+
     @GetMapping("/get_backup_worlds")
     public String getBackupWorlds(){
         File folder = new File(System.getProperty("user.dir"));
         File[] listOfFiles = folder.listFiles();
 
         for (int i = 0; i < listOfFiles.length; i++) {
+            template.convertAndSend("/logs", listOfFiles[i].getName());
             if (listOfFiles[i].getName().equals("mc_server")) {
                 System.out.println("File " + listOfFiles[i].getName());
                 return listOfFiles[i].getName();
